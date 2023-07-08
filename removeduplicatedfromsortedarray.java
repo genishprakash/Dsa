3587b78872585456c913705636481cde8d48d5ed
genishprakash <genishprakash@gmail.com>
2023-07-08 16:06:58 +0530

    remove dup from sorted array

---
+++ removeduplicatedfromsortedarray.java
@@ -0,0 +1,16 @@
+public class removeduplicatedfromsortedarray {
+    public int removeDuplicates(int[] nums) {
+        int count =1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]!=nums[i]){
+                nums[count]=nums[i];
+                count++;
+            }
+            else{
+
+            }
+            
+        }
+        return count;
+    }
+}
