303f30062ff4ddf7af795d1f08fbc87246d8e7d8
genishprakash <genishprakash@gmail.com>
2023-08-08 00:22:58 +0530

    Solution to the problem:Majority element

---
+++ majorityelement.java
@@ -0,0 +1,20 @@
+public class majorityelement {
+    public int majorityElement(int[] nums) {
+        int count=0,maximumoccurence=nums[0];
+         for(int i=0;i<nums.length;i++){
+             if(count==0){
+                 maximumoccurence=nums[i];
+                 count++;
+             }
+             else{
+                 if(maximumoccurence==nums[i]){
+                     count++;
+                 }
+                 else{
+                     count--;
+                 }
+             }
+         }
+         return maximumoccurence;
+    }
+}
